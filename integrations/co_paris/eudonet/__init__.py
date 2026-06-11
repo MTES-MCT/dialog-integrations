7bd78b54609f128b48d4a4632f12374c968ffa9c
Ju/smwhr <ju@smwhr.net>
2026-06-11 14:57:20 +0200

    [WIP]

--- integrations/co_paris/eudonet/__init__.py
+++ integrations/co_paris/eudonet/__init__.py
@@ -1,5 +1,6 @@
+from .eudonet_client import EudonetClient as EudonetClient
+from .eudonet_extractor import EudonetParisExtractor as EudonetParisExtractor
+
 from .data_source_integration import (
     ParisEudonetDataSourceIntegration as ParisEudonetDataSourceIntegration,
 )
-from .eudonet_client import EudonetClient as EudonetClient
-from .eudonet_extractor import EudonetParisExtractor as EudonetParisExtractor
